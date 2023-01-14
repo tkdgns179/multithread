da6ef592f7082023d005f13159e0fd8b71611d54
tkdgns179 <tkdgns179@gmail.com>
2023-01-14 16:58:20 +0900

    throughput

---
+++ src/com/example6_throughput/ThroughputHttpSever.java
@@ -0,0 +1,83 @@
+package com.example6_throughput;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpsServer;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import sun.net.httpserver.HttpsServerImpl;
+
+public class ThroughputHttpSever {
+    private static final String INPUT_FILE = "resources/throughput/war_and_peace.txt";
+    private static final int NUMBER_OF_THREADS = 2;
+
+    public static void main(String[] args) throws IOException {
+        String text = new String(Files.readAllBytes(Paths.get(INPUT_FILE)));
+        startServer(text);
+    }
+
+    public static void startServer(String text) throws IOException {
+
+        // backlog : http 서버 요청에 맞는 대기열 크기를 설정 -> 모든 요청이 스레드 풀 대기열에 들어가야하기 때문에 0으로 설정
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/search", new WordCountHandler(text));
+        Executor executor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+        server.setExecutor(executor);
+        server.start();
+
+    }
+
+    private static class WordCountHandler implements HttpHandler {
+        private String text;
+
+        public WordCountHandler(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            String query = httpExchange.getRequestURI().getQuery();
+            String[] keyValue = query.split("=");
+            String action = keyValue[0];
+            String word = keyValue[1];
+
+            if(!action.equals("word")) {
+                httpExchange.sendResponseHeaders(400, 0);
+                return;
+            }
+
+            long count = countWord(word);
+
+            byte[] response = Long.toString(count).getBytes();
+            httpExchange.sendResponseHeaders(200, response.length);
+            OutputStream outputStream = httpExchange.getResponseBody();
+            outputStream.write(response);
+            outputStream.close();
+
+        }
+
+        private long countWord(String word) {
+            long count = 0;
+            int index = 0;
+            while (index >= 0) {
+                index = text.indexOf(word, index);
+
+                if(index >= 0) {
+                    count++;
+                    index++;
+                }
+            }
+
+            return count;
+
+        }
+
+    }
+}
